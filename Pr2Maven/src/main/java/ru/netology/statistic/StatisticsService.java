28049c5b14098e56cfd4392fac2f5fd8f2c560dd
Olga Belianko <Obelyanko@gmail.com>
2021-06-19 21:45:34 +0300

    commit for src pom etc

---
+++ Pr2Maven/src/main/java/ru/netology/statistic/StatisticsService.java
@@ -0,0 +1,80 @@
+package ru.netology.statistic;
+
+public class StatisticsService {
+
+    public long calculateSum(long[] sales) {
+        long sum = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        return sum;
+    }
+
+    public long calculateAverage(long[] sales) {
+        long sum = 0;
+        long average = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        average = sum / sales.length;
+        return average;
+    }
+
+    public int maxSales(long[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(long[] sales) {
+        int minMonth = 0;
+        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
+        for (long sale : sales) {
+            // sales[minMonth] - продажи в месяце minMonth
+            // sale - продажи в рассматриваемом месяце
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
+        }
+        return minMonth + 1;
+    }
+
+    public long calculateLowAverageSales(long[] sales) {
+        long sum = 0;
+        long average = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        average = sum / sales.length;
+        int m = 0;
+        for (long sale : sales) {
+            if (sale < average) {
+                m = m + 1;
+            }
+        }
+        return m;
+    }
+
+    public long calculateUpAverageSales(long[] sales) {
+        long sum = 0;
+        long average = 0;
+        for (long sale : sales) {
+            sum += sale;
+        }
+        average = sum / sales.length;
+        int m = 0;
+        for (long sale : sales) {
+            if (sale > average) {
+                m = m + 1;
+            }
+        }
+        return m;
+    }
+}
---
+++ Pr2MavenStatistics/src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,8 @@
+package ru.netology.stats;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StatsServiceTest {
+
+}
